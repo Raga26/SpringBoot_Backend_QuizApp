f42a2f46e153b1c29f887213c727c5ef046293ca
Ragamaliga <mragamaliga@gmail.com>
2024-01-12 14:14:54 +0530

    first commit of quiz app

---
+++ springboot-quiz-app/src/test/java/com/example/springbootquizapp/SpringbootQuizAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootquizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootQuizAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
